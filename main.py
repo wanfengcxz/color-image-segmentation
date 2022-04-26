26a9bf8bae4cbc1282bb7e438ae04b7b96e11ece
Håvard Hjelmeseth <havhje@gmail.com>
2022-04-26 18:43:08 +0200

    feat: main GA file

--- main.py
+++ main.py
@@ -1,23 +1,45 @@
-from PIL import Image
-from numpy import asarray
-
+import numpy as np
+from population import Population
 from individual import Individual
-# load the image
-image = Image.open('training_images/train.png')
-# convert image to numpy array
-data = asarray(image)
-print(data[3][3])
+from tqdm.auto import trange, tqdm
+from PIL import Image
+from utilities import save_segmented_img
 
 
-ind = Individual((4, 4))
-ind.initialize_random()
-print(ind.genotype)
-print(ind.get_2d_array())
-print(ind.get_value_down(10))
+def genetic_algorithm(img_data, segment_constraints, population_size: int, num_generations: int = 40, debug=False) -> Individual:
+
+    # Initialize population
+    pop = Population(img_data, population_size, segment_constraints)
+    pop.initialize_population()
+
+    # Run evolution for num_generations
+    for _ in trange(num_generations):
+        if debug:
+            tqdm.write(str(pop.get_avg_fitness()))
+        pop.generate_new_population()
+
+    # Get best individual solution
+    return pop.get_best_individual()
 
 
-segmentation = [[0, 1, 2, 4, 5], [3, 6, 7, 10], [8, 9, 11, 12, 13, 14, 15]]
+if __name__ == "__main__":
+    # Variables
+    segment_constraints = {
+        "min": 3,
+        "max": 40
+    }
+    image_no = "86016"
 
+    # Load img data
+    img_data = np.asarray(Image.open(
+        "training_images/"+image_no+"/Test image.jpg"), dtype='int64')
 
-def get_deviation(segment):
-    pass
+    # Run GA
+    solution = genetic_algorithm(
+        img_data, segment_constraints=segment_constraints, population_size=80, num_generations=10
+    )
+
+    save_segmented_img("training_images/"+image_no+"/solutions/",
+                       img_data, solution.segments, 1)
+    save_segmented_img("training_images/"+image_no+"/solutions/",
+                       img_data, solution.segments, 2)
